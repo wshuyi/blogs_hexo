77acc655a493987101ccfcaa73c2dabf32b10756
Wang Shuyi <nkwshuyi@gmail.com>
2015-09-11 20:31:53 +0800

    first commit

---
+++ wsy/make_links_blog_wsy.sh
@@ -0,0 +1,10 @@
+#!/bin/zsh
+
+rm -f /Users/wsy/Documents/blog/wsy/_config.yml
+rm -f /Users/wsy/Documents/blog/wsy/source
+rm -f /Users/wsy/Documents/blog/wsy/themes
+
+ln -s /Users/wsy/Dropbox/blog/wsy/_config.yml /Users/wsy/Documents/blog/wsy/_config.yml
+ln -s /Users/wsy/Dropbox/blog/wsy/source /Users/wsy/Documents/blog/wsy/source
+ln -s /Users/wsy/Dropbox/blog/wsy/themes /Users/wsy/Documents/blog/wsy/themes
+
---
+++ wzc/make_links_blog_wzc.sh
@@ -0,0 +1,9 @@
+#!/bin/zsh
+
+rm -f /Users/wsy/Documents/blog/wzc/_config.yml
+rm -f /Users/wsy/Documents/blog/wzc/source
+rm -f /Users/wsy/Documents/blog/wzc/themes
+ln -s /Users/wsy/Dropbox/blog/wzc/_config.yml /Users/wsy/Documents/blog/wzc/_config.yml
+ln -s /Users/wsy/Dropbox/blog/wzc/source /Users/wsy/Documents/blog/wzc/source
+ln -s /Users/wsy/Dropbox/blog/wzc/themes /Users/wsy/Documents/blog/wzc/themes
+
